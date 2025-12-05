d42b53ee39b454b6dc66c63b6256c0a4dbcab9db
Yassin-amr-16001061 <yassin.hegazy@student.giu-uni.de>
2025-12-05 04:36:15 +0200

    Update Process class with new attributes and constructor
    Refactored Process class to include new attributes and constructor.

--- CPUSchedulingSimulator/CPUSchedulingSimulator/Process.h
+++ CPUSchedulingSimulator/CPUSchedulingSimulator/Process.h
@@ -21,6 +21,46 @@ public:
 };
 
 
+//
+// Created by Lenovo on 12/4/2025.
+//
+
+#ifndef OS_PROCESS_H
+#define OS_PROCESS_H
+#include<string>
+using namespace std;
+class Process
+{
+//the attributes of the Process class are for all the algorthimis unified for every one
+public:
+   string pid;
+    int arrivaltime;
+    int bursttime;
+    int priorty;
+    int remaning;
+    int startTime;
+    int compleTime;
+
+Process(const string id ,int arrival,int burst,int pr )
+{
+   pid=id;
+    arrivaltime=arrival;
+    bursttime=burst;
+    priorty=pr;
+    remaning=burst;// at the begining nothing happend so the remaining is the same as the start
+    startTime=-1;
+    compleTime=-1;//-1 denotes that the process hasnt finshed  yet
+}
+};
+
+
+#endif //OS_PROCESS_H
+
+
+
+
+
+
 
 
 
