0a564e301f291f5c67f15c7ecb991554040df2d6
ZeyadAmr-16005823 <zeyad.elserwy@student.giu-uni.de>
2025-12-05 10:34:24 +0200

    Add CSVReader constructor to load process data
    Implemented CSVReader constructor to read process data from a CSV file and populate a vector of Process objects.

--- CPUSchedulingSimulator/CPUSchedulingSimulator/CSVReader.h
+++ CPUSchedulingSimulator/CPUSchedulingSimulator/CSVReader.h
@@ -1,5 +1,10 @@
 #pragma once
 #include<iostream>
+using namespace std;
+#include "Process.h"
+#include<vector>
+#include <fstream>
+#include <sstream>
 
 
 #include<string>
@@ -7,11 +12,28 @@ class CSVReader
 {
 public:
 
-	//Constructor
-	CSVReader(std::string file_path, std::vector<Process>& Processes) {
 
-	}
+    CSVReader(string file_path, vector<Process>& Processes) {
+        ifstream file(file_path);
+        string line;
+        getline(file, line);
+        while (getline(file, line)) {
+            stringstream ss(line);
+            string pid, at, bt, pr;
+            getline(ss, pid, ',');
+            getline(ss, at, ',');
+            getline(ss, bt, ',');
+            getline(ss, pr, ',');
+            Process p;
+            p.pid = pid;
+            p.burstTime = stoi(bt);
+            p.priority = stoi(pr);
+            p.arrivalTime = stoi(at);
+
+            Processes.push_back(p);
+
+        }
+        file.close();
+    }
+
 };
-
-
-
